5594b43c2057328d5f203003451933dbab583fa6
senior9324 <senior9324@gmail.com>
2019-02-08 18:55:46 +0900

    버전 생성

--- mltd.notice.user.js
+++ mltd.notice.user.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name MLTD Notice
+// @version 0.1
 // @run-at document-idle
 // @match https://webview-dot-theaterdays.appspot.com/*
 // @grant GM_setClipboard
@@ -9,6 +10,7 @@
 // @grant GM.openInTab
 // @grant GM.xmlHttpRequest
 // @require https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
+// @downloadURL https://senior9324.github.io/mltd-notice/mltd.notice.user.js
 // ==/UserScript==
 
 (async function() {
@@ -16,7 +18,7 @@
     if (data.onload || data.onerror) {
       throw new Error('이 함수를 callback 패턴으로 쓰지 마세요.')
     } else {
-      new Promise((resolve, reject) => {
+      return new Promise((resolve, reject) => {
         GM.xmlHttpRequest(Object.assign(
           data,
           {
@@ -36,29 +38,14 @@
     })
 
     const { specialBg, bgList } = res.response
-    
-    /*const bgList = [
-      'http://imas.gamedbs.jp/mlth/image/card/bg/1524204991001_ygd895np.png',
-      'http://imas.gamedbs.jp/mlth/image/card/bg/1547964604001_i1s5ju0x.png',
-      'http://imas.gamedbs.jp/mlth/image/card/bg/1538546895001_7q81ksho.png',
-      'http://imas.gamedbs.jp/mlth/image/card/bg/1521526137002_6i8bp15c.png',
-      'http://imas.gamedbs.jp/mlth/image/card/bg/1545372667001_6u20n8lv.png',
-      'http://imas.gamedbs.jp/mlth/image/card/bg/1549087982001_5pjv72l6.png',
-      'http://imas.gamedbs.jp/mlth/image/card/bg/1540188595005_nbymk34u.png'
-    ]
-  
-    const specialBg = {
-      end: 1549799999999,
-      bg: 'http://imas.gamedbs.jp/mlth/image/card/bg/1549087982001_5pjv72l6.png'
-    }*/
-    
+
     if (specialBg.end && (Date.now() <= specialBg.end)) {
       return specialBg.bg
     } else {
       return bgList[Math.floor(Math.random() * bgList.length)]
     }
   }
-  
+
   function searchParentLink(elem, lastElem) {
     while (elem.nodeName !== 'A') {
       elem = elem.parentNode
@@ -68,16 +55,16 @@
     }
     return elem
   }
-  
+
   function removeAttributes(elem) {
     while (elem.attributes.length > 0)
       elem.removeAttribute(elem.attributes[0].name)
   }
-  
+
   async function getMainStylesheet() {
-  
+
   }
-  
+
   let stylesheet = `
   @keyframes hide {
     0% {
@@ -88,13 +75,13 @@
       visibility: hidden;
     }
   }
-  
+
   html, body {
     height: 100%;
     margin: 0;
     padding: 0;
   }
-  
+
   .bg {
     position: fixed;
     width: 100%;
@@ -108,12 +95,12 @@
     transform: scale(1.02);
     transition: transform 250ms;
   }
-  
+
   .bgmode .bg {
     filter: none;
     transform: scale(1);
   }
-  
+
   .container {
     position: relative;
     border-radius: 30px 30px 0 0;
@@ -126,37 +113,37 @@
     opacity: 1;
     transition: visibility 500ms, opacity 500ms;
   }
-  
+
   .bgmode .container {
     visibility: hidden;
     opacity: 0;
   }
-  
+
   header {
     background-image: linear-gradient(#feea8b, #dbc877, #c4b36b);
     border-radius: 30px 30px 0 0;
     -webkit-user-select: none;
     user-select: none;
   }
-  
+
   header h1 {
     float: left;
     margin: 0;
     padding: 10px 20px;
     font-size: 16px;
   }
-  
+
   #tablist {
     float: left;
     list-style: none;
     margin: 0;
     padding: 0;
   }
-  
+
   #tablist li {
     display: inline-block;
   }
-  
+
   #tablist li a {
     display: inline-block;
     margin-top: 6px;
@@ -165,26 +152,26 @@
     text-decoration: none;
     transform: skewX(8deg);
   }
-  
+
   #tablist li a .border {
     padding: 4px 20px 4px 20px;
     border-right: 1px solid black;
     display: inline-block;
   }
-  
+
   #tablist li:first-child a .border {
     border-left: 1px solid black;
   }
-  
+
   #tablist li a .border > span {
     transform: skewX(-8deg);
     display: inline-block;
   }
-  
+
   #tablist li a.click {
     background: white;
   }
-  
+
   .sharebtn {
     float: right;
     display: inline-block;
@@ -192,7 +179,7 @@
     color: black;
     text-decoration: none;
   }
-  
+
   iframe {
     clear: both;
     flex-grow: 1;
@@ -202,52 +189,52 @@
     border: 0;
   }
   `
-  
+
   if (unsafeWindow === unsafeWindow.parent) {
     let intendedPage = location.pathname + location.hash
-    
+
     if (!intendedPage.includes('info/google#') && !intendedPage.includes('info/apple#')) {
       intendedPage = '/info/google#/update'
     }
-    
+
     // Clear Data
     document.head.innerHTML = ''
     document.body.innerHTML = ''
-    
+
     removeAttributes(document.head)
     removeAttributes(document.body)
     removeAttributes(document.documentElement)
-    
+
     history.replaceState(null, 'お知らせ', '/')
-    
+
     let browserTitle = document.createElement('title')
     browserTitle.textContent = 'お知らせ'
     document.head.appendChild(browserTitle)
-    
+
     let style = document.createElement('style')
     style.textContent = stylesheet
     document.head.appendChild(style)
-    
+
     let bg = document.createElement('div')
     bg.className = 'bg'
     bg.addEventListener('click', function (e) {
       document.body.classList.toggle('bgmode')
     })
     document.body.appendChild(bg)
-    
+
     let container = document.createElement('div')
     container.className = 'container'
-    
+
     let iframe = document.createElement('iframe')
     iframe.src = intendedPage
     iframe.name = 'mainFrame'
-    
+
     let header = document.createElement('header')
-    
+
     let title = document.createElement('h1')
     title.textContent = 'お知らせ'
     header.appendChild(title)
-    
+
     let tablist = document.createElement('ul')
     tablist.id = 'tablist'
     tablist.innerHTML = '' +
@@ -256,7 +243,7 @@
       '<li><a href="/info/google#/event" target="mainFrame"><span class="border"><span>Event</span></span></a></li>'
     tablist.addEventListener('click', function (e) {
       let target = searchParentLink(e.target, tablist)
-      
+
       if (target) {
         if (target.className === 'click') {
           e.preventDefault();
@@ -266,9 +253,9 @@
         }
       }
     })
-    
+
     header.appendChild(tablist)
-    
+
     let share = document.createElement('a')
     share.className = 'sharebtn'
     share.innerHTML = 'Share'
@@ -279,26 +266,26 @@
       alert('복사가 완료되었습니다.')
     })
     header.appendChild(share)
-    
+
     container.appendChild(header)
     container.appendChild(iframe)
-    
+
     document.body.appendChild(container)
   } else {
     window.addEventListener('click', function (e) {
       let target = searchParentLink(e.target)
-      
+
       if (target && target.href.indexOf('browser:') === 0) {
         e.preventDefault()
-        
+
         GM.openInTab(target.href.substring(8))
       }
     })
-    
+
     let style = document.createElement('style')
-    
+
     style.textContent = '* { -webkit-user-select: auto !important; }'
-    
+
     document.head.appendChild(style)
-  }  
+  }
 })()
